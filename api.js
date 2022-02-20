09750a804673bc1b45f9e94475e05c7750a8764d
leemrtnzz <ichsan.haekal23@gmail.com>
2022-02-21 00:21:22 +0700

    first

---
+++ api.js
@@ -0,0 +1,20 @@
+const { readFileSync } = require('original-fs')
+const express = require('express')
+const app = express()
+
+
+const lokasi = JSON.parse(readFileSync('src/lokasi.json'))
+app.get('/lokasi', (req, res) => {
+    const page = req.query.page
+    const limit = req.query.limit
+
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+
+    const result = lokasi.slice(startIndex, endIndex)
+    res.json(result)
+})
+
+
+app.listen(8080)
+console.log('Server Sedang Jalan')
